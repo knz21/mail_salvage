3bc3d1a5479b3765388d2089c69e3e4c234ef14d
knz21 <cismollre@gmail.com>
2021-11-21 09:56:12 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+const query = ''
+const sheetId = ''
+const headers = ['id', 'date', 'from', 'to', 'cc', 'bcc', 'subject', 'plainBody', 'starred', 'unread']
+const fetchSize = 500
+
+const salvage = () => {
+    const sheet = SpreadsheetApp.openById(sheetId)
+    const queryResultSheet = sheet.getSheetByName(query) || sheet.insertSheet(query)
+    queryResultSheet.getRange(1, 1, 1, headers.length).setValues([headers])
+    const lastRow = queryResultSheet.getLastRow()
+    const existsIds = queryResultSheet.getRange(2, 1, lastRow).getValues().map(row => row[0])
+
+    const threads: GoogleAppsScript.Gmail.GmailThread[] = GmailApp.search(query, lastRow - 1, fetchSize)
+
+    Logger.log(`threads: ${threads.length}`)
+
+    const messages: String[][] = []
+    threads.forEach(thread => thread.getMessages().forEach((message: GoogleAppsScript.Gmail.GmailMessage) => {
+        const id = message.getId()
+        if (existsIds.includes(id)) return
+        messages.push([
+            message.getId(),
+            message.getDate().toString(),
+            message.getFrom(),
+            message.getTo(),
+            message.getCc(),
+            message.getBcc(),
+            message.getSubject(),
+            message.getPlainBody(),
+            message.isStarred() ? '⭐️' : '',
+            message.isUnread() ? '✉️' : ''
+        ])
+    }))
+
+    if (messages.length == 0) {
+        Logger.log('No results')
+        return
+    }
+
+    Logger.log(`messages: ${messages.length}`)
+    queryResultSheet.getRange(lastRow + 1, 1, messages.length, headers.length).setValues(messages)
+}
