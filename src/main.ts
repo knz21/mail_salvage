0a97b3eb610dd063ad9943ad191f36f020d785e7
knz21 <cismollre@gmail.com>
2021-12-05 19:37:38 +0900

    config, reduce rows, divide sheets

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
-const query = ''
 const sheetId = ''
-const headers = ['id', 'date', 'from', 'to', 'cc', 'bcc', 'subject', 'plainBody', 'starred', 'unread']
-const fetchSize = 500
-const cellMaxLength = 50000
+const headers = ['id', 'date', 'from', 'to', 'cc', 'bcc', 'subject', 'plainBody']
+const defaultFetchSize = 500
+const defaultMaxRowSize = 5000
+const defaultConfig = [['query', ''], ['fetch_size', defaultFetchSize], ['max_row_size', defaultMaxRowSize]]
+const maxCellTextLength = 50000
 const format = 'yyyy/mm/dd hh:mm:ss'
+const configSheetName = 'config'
 const resumablesSheetName = 'resumables'
 
 const salvage = () => {
     const sheet = SpreadsheetApp.openById(sheetId)
-    const queryResultSheet = sheet.getSheetByName(query) || sheet.insertSheet(query)
+    const configSheet = sheet.getSheetByName(configSheetName)
+    if (!configSheet) {
+        Logger.log('The sheet "config" is necessary.')
+        sheet.insertSheet(configSheetName).getRange(1, 1, defaultConfig.length, defaultConfig[0].length).setValues(defaultConfig)
+        return
+    }
+    const configs = configSheet.getRange(1, 1, configSheet.getLastRow(), 2).getValues()
+    const query = configs.find(row => row[0] == 'query')?.[1]
+    if (!query) {
+        Logger.log('The value "query" is necessary in config.')
+        return
+    }
+    const fetchSize = configs.find(row => row[0] == 'fetch_size')?.[1] || defaultFetchSize
+    const maxRowSize = configs.find(row => row[0] == 'max_row_size')?.[1] || defaultMaxRowSize
+
+    let queryResultSheet = sheet.getSheetByName(query) || sheet.insertSheet(query)
     queryResultSheet.getRange(1, 1, 1, headers.length).setValues([headers])
-    const lastRow = queryResultSheet.getLastRow()
+    let lastRow = queryResultSheet.getLastRow()
     const existsIds = queryResultSheet.getRange(2, 1, lastRow).getValues().map(row => row[0])
 
     const resumablesSheet = sheet.getSheetByName(resumablesSheetName) || sheet.insertSheet(resumablesSheetName)
@@ -18,7 +35,6 @@ const salvage = () => {
     const resumables = resumablesSheet.getRange(1, 1, lastResumableRow > 0 ? lastResumableRow : 1, 2).getValues()
     const startIndex = resumables.find(row => row[0] == query)?.[1] || 0
     const threads: GoogleAppsScript.Gmail.GmailThread[] = GmailApp.search(query, startIndex, fetchSize)
-
     Logger.log(`threads: ${threads.length}`)
 
     const messages: any[][] = []
@@ -34,11 +50,27 @@ const salvage = () => {
             message.getCc(),
             message.getBcc(),
             message.getSubject(),
-            body.length < cellMaxLength ? body : `${body.substring(0, cellMaxLength - 20)}...${cellMaxLength}字を超えたので省略`,
-            message.isStarred() ? '⭐️' : '',
-            message.isUnread() ? '✉️' : ''
+            body.length < maxCellTextLength ? body : `${body.substring(0, maxCellTextLength - 50)}... Omitted because of max characters(${maxCellTextLength}).`
         ])
     }))
+
+    if (lastRow > 1 && lastRow + messages.length > maxRowSize) {
+        const numbers = sheet.getSheets()
+            .map(sheet => {
+                const name = sheet.getName()
+                const found = name.match(/#[0-9]+?$/)
+                if (name.startsWith(query) && found != null) {
+                    return parseInt(found[0].substring(1))
+                } else {
+                    return 0
+                }
+            })
+        const nextNumber = Math.max(...numbers) + 1
+        queryResultSheet.setName(`${query}#${nextNumber}`)
+        queryResultSheet = sheet.insertSheet(query)
+        queryResultSheet.getRange(1, 1, 1, headers.length).setValues([headers])
+        lastRow = 1
+    }
 
     if (messages.length == 0) {
         Logger.log('No results')
